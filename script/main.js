9469222b90a5617fa6ccd44b5dfe5e132c168b74
am-mahmud <am.mahmudasif@gmail.com>
2025-09-07 15:11:42 -0400

    Categories API added

--- script/main.js
+++ script/main.js
@@ -1,10 +1,18 @@
+// Useable function
+const removeActive = () =>{
+    const lessonButtons = document.querySelectorAll(".btn-cat")
+    lessonButtons.forEach((btn) => btn.classList.remove("active"))
+}
+
+
+// Loading the categories
 const loadCategories = () =>{
     fetch ("https://openapi.programming-hero.com/api/categories")
     .then ((res) => res.json())
     .then((json) => displayCategories(json.categories))
 }
 
-
+// All categories display via API
 const displayCategories = (categories) => {
     console.log(categories);
 
@@ -14,12 +22,48 @@ const displayCategories = (categories) => {
      for(let category of categories){
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button id = "cat-btn-${category.category_name}" onclick="loadCatPlant(${category.level_no})" class="py-1">
+        <button id = "cat-btn-${category.category_name}" onclick="loadCatPlant(${category.level_no})" class="py-1 btn-cat">
             ${category.category_name}
         </button>
         `;
         catContainer.appendChild(btnDiv);
     }
+}
+
+// Loading Plants based on categories
+const loadCatPlant = (id) =>{
+    const catsUrl = `https://openapi.programming-hero.com/api/category/${id}`;
+
+    fetch(catsUrl)
+    .then((res) => res.json())
+    .then((catPlant) => {
+
+        removeActive()
+        const clickBtn = document.getElementById(`cat-btn-${id}`)
+        clickBtn.classList.add("active");
+
+        displayLevelWord(catPlant.categories);
+
+    })    
+    
+}
+
+//All plant display via API
+const displayCatPlant = (words) => {
+
+    const wordContainer = document.getElementById("plants-container")
+    wordContainer.innerHTML = "";
+
+    plants.forEach(plant => {
+        
+       const plantCard = document.createElement("div")
+       plantCard.innerHTML = `
+          
+       `
+
+       wordContainer.appendChild(wordCard)
+    });
+
 }
 
 loadCategories();
